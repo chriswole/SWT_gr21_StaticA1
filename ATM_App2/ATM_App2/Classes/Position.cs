ef99f11a10b24df57a724c946f19f8bc617f88f2
Mette <mettechrisjacobsen@hotmail.com>
2018-11-08 09:46:57 +0100

    Added and saved the changes from before

--- ATM_App2/ATM_App2/Classes/Position.cs
+++ ATM_App2/ATM_App2/Classes/Position.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using System.Data;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ATM_App2.Classes
 {
-    class Position
+
+    public class Position
     {
+        public Position(long x = 0, long y = 0)
+        {
+            x_ = x;
+            y_ = y;
+        }
+
+        public long x_ { get; set; }
+        public long y_ { get; set; }
+
+        public static Position operator -(Position pos1, Position pos2)
+        {
+            Position pos3 = new Position();
+            pos3.x_ = pos1.x_ - pos2.x_;
+            pos3.y_ = pos1.y_ - pos2.y_;
+            return pos3;
+        }
+
+        public static bool operator >=(Position pos1, Position pos2)
+        {
+            return pos1.x_ >= pos2.x_ && pos1.y_ >= pos2.y_;
+        }
+
+        public static bool operator <=(Position pos1, Position pos2)
+        {
+
+            return pos1.x_ <= pos2.x_ && pos1.y_ <= pos2.y_;
+        }
+
+        //public void print()
+        //{
+        //    Console.WriteLine($"x = {x_}, y = {y_}");
+        //}
+
+
     }
 }
+
+
+
--- ATM_App2/ATM_App2/Classes/Track.cs
+++ ATM_App2/ATM_App2/Classes/Track.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using ATM_App2.Classes;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ATM_App2
+
+namespace ATM_App2.Classes
 {
-    class Track
+    public class Track //make track
     {
+        public Track(string tag = "", Position pos = null, int altitude = 0, int horiVelocity = 0, int course = 0,
+            string timestamp = "")
+        {
+            tag_ = tag;
+            pos_ = pos == null ? new Position(0, 0) : pos;
+            altitude_ = altitude;
+            hori_velocity_ = horiVelocity;
+            course_ = course;
+            timestamp_ = timestamp;
+        }
+
+        public string tag_ { get; set; }
+        public Position pos_ { get; set; }
+        public int altitude_ { get; set; }
+        public int hori_velocity_ { get; set; }
+        public int course_ { get; set; }
+        public string timestamp_ { get; set; }
+
+
+        /*
+    public override string ToString()
+        {
+            string str = $"Track:\n " +
+                         $"{tag_}\n " +
+                         $"{pos_.x_},{pos_.y_}\n " +
+                         $"{altitude_}\n " +
+                         $"{hori_velocity_}\n " +
+                         $"{timestamp_}\n";
+            return str;
+        }*/
+
+        public static bool operator ==(Track track1, Track track2)
+        {
+
+            if (track1.tag_ == track2.tag_ &&
+                track1.pos_.x_ == track2.pos_.x_ &&
+                track1.pos_.y_ == track2.pos_.y_ &&
+                track1.altitude_ == track2.altitude_ &&
+                track1.hori_velocity_ == track2.hori_velocity_ &&
+                track1.course_ == track2.course_ &&
+                track1.timestamp_ == track2.timestamp_)
+                return true;
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public static bool operator !=(Track track1, Track track2)
+        {
+
+            if (track1.tag_ == track2.tag_ &&
+                track1.pos_.x_ == track2.pos_.x_ &&
+                track1.pos_.y_ == track2.pos_.y_ &&
+                track1.altitude_ == track2.altitude_ &&
+                track1.hori_velocity_ == track2.hori_velocity_ &&
+                track1.course_ == track2.course_ &&
+                track1.timestamp_ == track2.timestamp_)
+                return false;
+            else
+            {
+                return true;
+            }
+
+        }
+
     }
 }
+
+
