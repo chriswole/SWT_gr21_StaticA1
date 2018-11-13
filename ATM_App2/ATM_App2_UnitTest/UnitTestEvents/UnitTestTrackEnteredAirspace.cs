059fac8e370f47ac0e5977bbdaa16e68f319a4ec
Mette <mettechrisjacobsen@hotmail.com>
2018-11-13 10:46:17 +0100

    Added folder for UnittestEvents, and started on UnitTestTrackEnterdAirspace

---
+++ ATM_App2/ATM_App2_UnitTest/UnitTestEvents/UnitTestTrackEnteredAirspace.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+//using NSubstitute;
+
+//namespace ATM_App2_UnitTest.UnitTestEvents
+//{
+//    [TestFixture]
+//    [Author("Mette Jacobsen, Edited by members of group 21")]
+//    [TestClass]
+//    public class UnitTestTrackEnteredAirspace
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
