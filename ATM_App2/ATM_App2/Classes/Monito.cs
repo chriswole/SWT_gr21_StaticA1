c4ed091a614b2ebff8be0c9adb468435ebd526ca
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-11-14 09:58:24 +0100

    Added Monito class, due to local issues, it can't be called Monitor for now - also added Pseudo code for this class until correct events are added

---
+++ ATM_App2/ATM_App2/Classes/Monito.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_App2.Classes
+{
+    class Monito //Temporary name until merge is fixed
+    {
+        public void InitMonitor()
+        {
+
+        }
+
+        public void UpdateMonitor()
+        {
+            /*
+            //Subscribing to inOutTrack events
+            inOutTrack whenSomethingEntersOrLeavesAirspace = new inOutTrack();
+            whenSomethingEntersAirspace.addPlaneToAirspace += UpdateMonitor;
+            whenSomethingLeavesAirspace.removePlaneFromAirspace -=UpdateMonitor; //Unsubscribing from inOutTrack event
+            
+             //Subscribing to Separation
+             Separation listAllActiveDangers = new Separation();
+             Separation.addActiveDanger += UpdateMonitor; //Subscribing to Separation event
+             Separation.removeActiveDanger -= UpdateMonitor //Unsubscribing from Separation event
+             */
+        }
+    }
+}
