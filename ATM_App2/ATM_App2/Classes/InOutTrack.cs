8575af8869d569f8b85250479d697b87e7c476b3
Mette <mettechrisjacobsen@hotmail.com>
2018-11-15 14:14:27 +0100

    Class InOutTrack added

--- ATM_App2/ATM_App2/Classes/InOutTrack.cs
+++ ATM_App2/ATM_App2/Classes/InOutTrack.cs
@@ -8,21 +8,33 @@ namespace ATM_App2.Classes
 {
     class InOutTrack
     {
-        //listoftrackincomming()
-        //{
-        //    List<Track>
-        //    instret()
+       /* 
+        public void OnEnteredTrack(object sender, TrackArgs totrack)
+        {
+            //do stuff
+
+        }
+
+        public void OnLeavingTrack(object sender, TrackArgs totrack)
+        {
+            //do stuff
+        }
+
+        //    listoftrackincomming()
         //    {
-        //       add To list if event called
-        // and make event for monitor
+        //        List<Track>
+        //        instret()
+        //        {
+        //            add To list if event called
+        //      and make event for monitor
         //    }
-        //    
+
         //    Remove()
         //    {
         //        remove from list if event
         //          and make new evnte
-        //    }
-
         //}
+        */
     }
 }
+
