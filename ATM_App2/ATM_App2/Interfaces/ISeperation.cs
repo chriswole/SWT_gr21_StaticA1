9e8ded982e4562781e0390ffd960e1c3ef04f1fa
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-11-15 10:12:42 +0100

    Added ISeperation and the real Monitor class has claimed it's name

--- ATM_App2/ATM_App2/Interfaces/ISeperation.cs
+++ ATM_App2/ATM_App2/Interfaces/ISeperation.cs
@@ -1,7 +1,19 @@
-﻿namespace ATM_App2.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM_App2.Classes;
+
+namespace ATM_App2.Interfaces
 {
     public interface ISeperation
     {
-        
+        List<Danger> newDangers_ { get; set; }
+        List<Danger> oldDangers_ { get; set; }
+
+        void calculateDistances(List<Track> tracklist);
+        void raiseAlarm();
+        void deactivateAlarm();
     }
 }
