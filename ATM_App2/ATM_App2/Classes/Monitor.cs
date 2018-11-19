e85b99065d742208568713c3549ccf63c4b50e2f
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-11-19 15:17:26 +0100

    Monitor subscribing to InOutTrack events and Separation events

--- ATM_App2/ATM_App2/Classes/Monitor.cs
+++ ATM_App2/ATM_App2/Classes/Monitor.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using ATM_App2.Classes;
+using ATM_App2.Events;
 
 namespace ATM_App2.Classes
 {
@@ -11,7 +16,7 @@ namespace ATM_App2.Classes
         //Constructor
         public Monitor(string test_)
         {
-            test_ = "Is full";
+
         }
 
         public void InitMonitor()
@@ -25,25 +30,49 @@ namespace ATM_App2.Classes
 
             else
             {
-                Console.Write("There are currently no planes in the airspace\n");
+                Console.WriteLine("There are currently no planes in the airspace\n");
             }
+
+            Console.WriteLine(true);
 
         }
 
         public void UpdateMonitor()
         {
-            /*
-            //Subscribing to inOutTrack events
-            inOutTrack whenSomethingEntersOrLeavesAirspace = new inOutTrack();
-            whenSomethingEntersAirspace.addPlaneToAirspace += UpdateMonitor;
-            whenSomethingLeavesAirspace.removePlaneFromAirspace -=UpdateMonitor; //Unsubscribing from inOutTrack event
+
+            //Subscribing to planes entering the airspace
+            InOutTrack entering = new InOutTrack();
+            entering.listInUpdated += new System.EventHandler<EnteredTrackArgs>(OnEntered);
             
-             //Subscribing to Separation
-             Separation listAllActiveDangers = new Separation();
-             Separation.addActiveDanger += UpdateMonitor; //Subscribing to Separation event
-             Separation.removeActiveDanger -= UpdateMonitor //Unsubscribing from Separation event
-             */
+            //Subscribing to planes leaving the airspace
+            InOutTrack leaving = new InOutTrack();
+            leaving.listOutUpdated += new System.EventHandler<LeftTrackArgs>(OnDeleted);
+
+
+            //Subscribing to Separation events
+            Separation monitoring = new Separation(new LogToFile());
+            monitoring.DangerListUpdated += new EventHandler<DangerlistArgs>(OnNewDanger);
+            
+
         }
-    
+
+        
+        private void OnEntered(object sender, EnteredTrackArgs e)
+        {
+            //Listing planes entering airspace
+            Console.WriteLine("{0} has entered til airspace", e.listEntered);
+        }
+
+        private void OnDeleted(object sender, LeftTrackArgs e)
+        {
+            //Listing planes leaving airspace
+            Console.WriteLine("{0} has left the airspace", e.listLeft);
+        }
+
+        private void OnNewDanger(object sender, DangerlistArgs e)
+        {
+            //Listing available dangers
+            Console.WriteLine("{0} are too close!", e.DangerList_);
+        }
     }
 }
