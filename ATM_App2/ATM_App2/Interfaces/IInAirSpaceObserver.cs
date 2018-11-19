14e4bbd4417ca403c416aed9da9fde4b8c69b87a
Kasper Andersen <kga@live.dk>
2018-11-19 08:11:52 +0100

    InAirSpace and interface files renamed and cleaned

--- ATM_App2/ATM_App2/Interfaces/IInAirSpaceObserver.cs
+++ ATM_App2/ATM_App2/Interfaces/IInAirSpaceObserver.cs
@@ -7,11 +7,7 @@ using ATM_App2.Classes;
 
 namespace ATM_App2.Interfaces
 {
-<<<<<<< HEAD
     interface IInAirSpaceObserver
-=======
-   public interface IInAirSpace
->>>>>>> 21deeb8977d32b20482f9a5b75087353a3076b96
     {
         event EventHandler<AirspaceTrackArgs> AirspaceUpdated;
         event  EventHandler<TrackArgs> EnteredTrack;
