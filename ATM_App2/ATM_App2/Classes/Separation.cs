b67a2f98981810ddf7e96d2e9b464a70d2091415
Mette <mettechrisjacobsen@hotmail.com>
2018-11-20 14:27:11 +0100

    added IInOutTrack, Unit test on track enteret and left airspace are out commet

--- ATM_App2/ATM_App2/Classes/Separation.cs
+++ ATM_App2/ATM_App2/Classes/Separation.cs
@@ -13,93 +13,90 @@ namespace ATM_App2.Classes
     {
         public List<Danger> DangerList_ { get; set; }
 
-    
+
     }
 
- 
-        public class Separation //: ISeparation
+
+    public class Separation //: ISeparation
+    {
+        private List<Danger> Dangers_ { get; set; }
+        private ITrackOpticsProvider opticsProvider_;
+        private ILogToFile loggerToFile_;
+
+
+        // Eventhandler to subscribe on
+        public EventHandler<DangerlistArgs> DangerListUpdated;
+        public void OnAirspaceUpdated(object sender, AirspaceTrackArgs argList)
         {
-            private List<Danger> Dangers_ { get; set; }
-            private ITrackOpticsProvider opticsProvider_;
-            private ILogToFile loggerToFile_;
+            CalculateDistances(argList.TracksInAirSpace);
+        }
 
 
-            // Eventhandler to subscribe on
-            public EventHandler<DangerlistArgs> DangerListUpdated;
-            public void OnAirspaceUpdated(object sender, AirspaceTrackArgs argList)
+        public Separation(ILogToFile logger2File, ITrackOpticsProvider opticsProvider)
+        {
+            Dangers_ = new List<Danger>();
+            opticsProvider_ = opticsProvider;
+            //?? returns the left side if not null, if null right side is returned.
+            loggerToFile_ = logger2File ?? new LogToFile();
+        }
+
+
+        public void CalculateDistances(List<Track> trackList)
+        {
+            var newTrack = trackList[0];
+            if (Dangers_.Count != 0)
             {
-                CalculateDistances(argList.TracksInAirSpace);
+                Dangers_ = opticsProvider_.RemoveOldDangers(newTrack, Dangers_);
             }
 
-       
-            public Separation(ILogToFile logger2File, ITrackOpticsProvider opticsProvider)
+            foreach (var track1 in trackList)
             {
-                Dangers_ = new List<Danger>();
-                opticsProvider_ = opticsProvider;
-                //?? returns the left side if not null, if null right side is returned.
-                loggerToFile_ = logger2File ?? new LogToFile();
-            }
-
-
-            public void CalculateDistances(List<Track> trackList)
-            {
-                var newTrack = trackList[0];
-                if (Dangers_.Count!=0)
+                // Are the planes within same altitude layer
+                int alt = Math.Abs(newTrack.altitude_ - track1.altitude_);
+                if (alt < 300)
                 {
-                    Dangers_ = opticsProvider_.RemoveOldDangers(newTrack, Dangers_);
-                }
-               
-                foreach (var track1 in trackList)
-                {
-                    // Are the planes within same altitude layer
-                    int alt = Math.Abs(newTrack.altitude_ - track1.altitude_);
-                    if (alt < 300)
+                    // are the planes too close in xy-plane
+                    Position dist = newTrack.pos_ - track1.pos_;
+                    var distance = Math.Sqrt((dist.x_ * dist.x_) + (dist.y_ * dist.y_));
+                    if (distance < 5000)
                     {
-                        // are the planes too close in xy-plane
-                        Position dist = newTrack.pos_ - track1.pos_;
-                        var distance = Math.Sqrt((dist.x_ * dist.x_) + (dist.y_ * dist.y_));
-                        if (distance < 5000)
+                        // Is the planes actually the same - if not make a dangerObj
+                        if (newTrack.tag_ != track1.tag_)
                         {
-                            // Is the planes actually the same - if not make a dangerObj
-                            if (newTrack.tag_ != track1.tag_)
+                            int i = 0;
+                            Danger dangerObj = new Danger(newTrack, track1, (int)distance);
+                            foreach (var dangerObject in Dangers_)
                             {
-                                int i = 0;
-                                Danger dangerObj = new Danger(newTrack, track1, (int) distance);
-                                foreach (var dangerObject in Dangers_)
+                                if (dangerObj.track1_ == dangerObject.track1_ && dangerObj.track2_ == dangerObject.track2_)
                                 {
-                                    if (dangerObj.track1_ == dangerObject.track1_ && dangerObj.track2_ == dangerObject.track2_)
-                                    {
-                                        Dangers_[i] = dangerObj;
-                                    }
-                                    else
-                                    {
-                                        Dangers_.Add(dangerObj);
+                                    Dangers_[i] = dangerObj;
+                                }
+                                else
+                                {
+                                    Dangers_.Add(dangerObj);
 
-                                    }
-
-                                    i++;
                                 }
 
+                                i++;
                             }
+
                         }
                     }
-                    // lav Danger list updated event her. 
-                    OnDangerListUpdated(Dangers_);
-                    
                 }
-            }
-            
-            
-            protected virtual void OnDangerListUpdated(List<Danger> dangers)
-            {
-                if (DangerListUpdated != null)
-                {
-                    DangerListUpdated(this, new DangerlistArgs(){DangerList_ = dangers});
-                }
-            }
-            
+                // lav Danger list updated event her. 
+                OnDangerListUpdated(Dangers_);
 
-
+            }
         }
-   
+
+
+        protected virtual void OnDangerListUpdated(List<Danger> dangers)
+        {
+            if (DangerListUpdated != null)
+            {
+                DangerListUpdated(this, new DangerlistArgs() { DangerList_ = dangers });
+            }
+        }
+
+    }
 }
