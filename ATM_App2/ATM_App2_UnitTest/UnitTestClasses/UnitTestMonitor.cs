43c3a44bd855003492e85fa96a012d52ce7eede9
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-11-22 00:05:08 +0100

    Added possible test code for monitor, need to double check with group if necessary at all

--- ATM_App2/ATM_App2_UnitTest/UnitTestClasses/UnitTestMonitor.cs
+++ ATM_App2/ATM_App2_UnitTest/UnitTestClasses/UnitTestMonitor.cs
@@ -22,16 +22,19 @@ namespace ATM_App2_UnitTest.UnitTestClasses
     class UnitTestMonitor
     {
 
-        private Monitor _uut;
+        private InOutTrack _uut;
         private IInAirSpaceObserver fakeAirSpace_;
+        private ILogToFile log_;
 
         [SetUp]
         public void Setup()
         {
             fakeAirSpace_ = Substitute.For<IInAirSpaceObserver>();
-            _uut = new Monitor();
+            log_ = Substitute.For<ILogToFile>();
+            _uut = new InOutTrack();
 
-
+            fakeAirSpace_.EnteredTrack += _uut.OnEnteredTrack;
+            fakeAirSpace_.LeavingTrack += _uut.OnLeavingTrack;
 
         }
 
@@ -58,7 +61,53 @@ namespace ATM_App2_UnitTest.UnitTestClasses
 
             Assert.That(updatedAirspaceMonitored.Any( x => airspaceMonitored.Any(y => x.Equals(y))));
         }
+        /*
+        [Test]
+        public void PlanesEnteringAirspace()
+        {
 
+
+            
+            List<Track> recievedPlane = new List<Track>();
+            InOutTrack updateMonitor = new InOutTrack();
+
+
+
+            updateMonitor.listInUpdated += delegate(object sender, EnteredTrackArgs e)
+            {
+                recievedPlane.Add(_uut.OnEnteredTrack());
+            };
+
+            Assert.AreEqual(2, recievedPlane.Count);
+            Assert.AreEqual("RecievedPlane", recievedPlane[0]);
+
+            
+
+            
+            InOutTrack entering = new InOutTrack();
+            
+
+            
+            InOutTrack leaving = new InOutTrack();
+            leaving.listOutUpdated += new System.EventHandler<LeftTrackArgs>(OnDeleted);
+
+
+            
+            monitoring.DangerListUpdated += new EventHandler<DangerlistArgs>(OnNewDanger);
+            
+        }
+
+        private void OnEntered(object sender, EnteredTrackArgs e)
+        {
+        }
+
+        private void OnDeleted(object sender, LeftTrackArgs e)
+        {
+        }
+
+        private void OnNewDanger(object sender, DangerlistArgs e)
+        {
+        }*/
     }
 
 
